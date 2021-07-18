90f59b3c0fd2cda1d0553f859de2bf1475984dee
fredyflemus <fredyflemus@gmail.com>
2021-07-17 23:04:37 -0600

    travis fix

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
             {
                 patterns: [{
                     from: './src/styles/styles.css',
-                    to: ''
+                    to: './styles,css'
                 }],
             },
         ),
